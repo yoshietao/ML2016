905e7169a313f28f53241e69e5fdf04651ca2723
yoshie <b02502108@ntu.edu.com.tw>
2016-10-26 19:02:11 +0800

    temp data

--- hw2/test.py
+++ hw2/test.py
@@ -2,16 +2,12 @@ import numpy as np
 import sys
 import math
 
+def sigmoid(z):
+	return 1/(1+math.exp(-z))
+
 model 	 = sys.argv[1]
 testdata = sys.argv[2]
 output   = sys.argv[3]
-
-def Pxc(x,miu,var):
-	pxc = 1
-	for i in range(1,58):
-		if var[i] != 0 :
-			pxc = pxc*(float(1)/(2*math.pi)**0.5)*(float(1)/var[i]**0.5)*math.exp(-1*(x[i]-miu[i])**2/(2*var[i]))
-	return pxc
 
 data = []
 f = open(model,'r')
@@ -20,19 +16,12 @@ for line in f:
 	data.append(line.strip().split(','))
 f.close()
 
-#data[0]:pc0
-#data[1]:pc1
-#data[2]:mean0
-#data[3]:mean1
-#data[4]:variance0
-#data[5]:variance1
+#data[0]:w
+#data[1]:b
 
-pc0 = float(data[0][0])
-pc1 = float(data[1][0])
-mean0 = np.array([float(j) for j in data[2]])
-mean1 = np.array([float(j) for j in data[3]])
-variance0 = np.array([float(j) for j in data[4]])
-variance1 = np.array([float(j) for j in data[5]])
+W = np.array([float(j) for j in data[0]])
+b = float(data[1][0])
+
 
 testx = []
 f = open(testdata,'r')
@@ -44,12 +33,12 @@ f.close()
 fout = open(output,'w')
 fout.write('id,label\n')
 for i in range(600):
-	X = [float(j) for j in testx[i]]
-	ypredict = (Pxc(X,mean0,variance0))/(Pxc(X,mean0,variance0)+Pxc(X,mean1,variance1))
+	X = np.array([float(j) for j in testx[i]])
+	ypredict = sigmoid(np.dot(X,W)+b)
 	if ypredict >= 0.5:
-		ypredict = 0
-	else:
 		ypredict = 1
+	else:
+		ypredict = 0
 	fout.write(str(i+1))
 	fout.write(',')
 	fout.write(str(ypredict))
